a07ae023b07a52980d5efc1fae89ae3500d3f6a9
Afri3nd <logant2@uci.edu>
2024-04-21 01:09:30 -0700

    Sign up implemented

--- GeoTraveler/Backend/index.js
+++ GeoTraveler/Backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+
 app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.post("/", async (req, res) => {
--- GeoTraveler/src/Components/SignUp-Login/SignUp.jsx
+++ GeoTraveler/src/Components/SignUp-Login/SignUp.jsx
@@ -1,14 +1,31 @@
 import "./SignUp.css";
 import background from "../../assets/greenMountains.jpg";
 export default function SignUp() {
+  async function signupDo(){ // GOOD
+    const username = document.getElementById("usernameInput").value;
+    const password = document.getElementById("passwordInput").value;
+    const email = document.getElementById("emailInput").value;
+    const age = document.getElementById("ageInput").value;
+    const concat = username + password;
+    const hash = btoa(concat);
+    console.log(JSON.stringify({ hash }));
+    const postresponse = await fetch("http://localhost:3001/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ hash, username, email, age}),
+    }).then((res) => res.json());
+  }
+
   return (
     <div className="signup-container">
       <div className="signup">
         <h1 style={{ color: "white" }}>Sign Up</h1>
-        <input placeholder="Name" />
-        <input placeholder="Email" />
-        <input placeholder="Age" />
-        <input placeholder="Password" />
+        <input id="usernameInput" placeholder="Name" />
+        <input id="emailInput" placeholder="Email" />
+        <input id="ageInput" placeholder="Age" />
+        <input id="passwordInput" placeholder="Password" />
         <input placeholder="Confirm Password" />
         <button>Submit</button>
       </div>
